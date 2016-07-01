11875e3397c5e4cdc8bb8eb60fcc9bab86dd27dd
William Youngc <william@werl.me>
2016-06-30 21:43:02 -0400

    Terrible Commit

--- application.js
+++ application.js
@@ -21,21 +21,28 @@ $(function() {
   })
 
 $('#map').on('idle',function(){
-  console.log("idle.. meh!!!!");
-  console.log(map.getCenter().lat());
-  console.log(map.getCenter().lng());
+  var lat = map.getCenter().lat();
+  var lng = map.getCenter().lng();
+  var bounds = map.getBounds();
+  var center = map.getCenter();
+  if (bounds && center) {
+      var ne = bounds.getNorthEast();
+        // Calculate radius (in meters).
+        var radius = google.maps.geometry.spherical.computeDistanceBetween(center, ne);
+  }
+  $('#black-box > img').remove();
+  _500px.api('/photos/search', { geo: lat + ',' + lng + ',' + radius/1000 + 'km'}, function (pages){
+      console.log(pages);
+      for(var i = 1; i <= pages.data.total_pages && i<=5; i++){
+      _500px.api('/photos/search', { geo: lat + ',' + lng + ',' + radius/1000 + 'km', page: i }, function (response) {
+        photo_array = response.data.photos.forEach(function(photo){
+          $("<img>").attr("src", photo.image_url).appendTo("#black-box")
+        });
+      });
+    };
+  })
 })
 
-_500px.api('/photos/search', { geo: '48.424138, -123.363203,5km'}, function (pages){
-    console.log(pages);
-    for(var i = 1; i <= pages.data.total_pages && i<=5; i++){
-    _500px.api('/photos/search', { geo: '48.424138, -123.363203,5km', page: i }, function (response) {
-      photo_array = response.data.photos.forEach(function(photo){
-        $("<img>").attr("src", photo.image_url).appendTo("#black-box")
-      });
-    });
-  };
-})
 
 
 })
