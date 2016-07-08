b3d022eca115e82be1a82cbedc52638bdfe13274
William Youngc <william@werl.me>
2016-07-07 20:20:34 -0400

    THIS IS BAD!!!

--- application.js
+++ application.js
@@ -20,28 +20,49 @@ $(function() {
     $(this).toggle( "slide" );
   })
 
-$('#map').on('idle',function(){
-  var lat = map.getCenter().lat();
-  var lng = map.getCenter().lng();
-  var bounds = map.getBounds();
-  var center = map.getCenter();
-  if (bounds && center) {
+  $('#map').on('idle',function(){
+
+    var lat = map.getCenter().lat();
+    var lng = map.getCenter().lng();
+    var bounds = map.getBounds();
+    var center = map.getCenter();
+    if (bounds && center) {
       var ne = bounds.getNorthEast();
-        // Calculate radius (in meters).
-        var radius = google.maps.geometry.spherical.computeDistanceBetween(center, ne);
-  }
-  $('#black-box > img').remove();
-  _500px.api('/photos/search', { geo: lat + ',' + lng + ',' + radius/1000 + 'km'}, function (pages){
-      console.log(pages);
-      for(var i = 1; i <= pages.data.total_pages && i<=5; i++){
-      _500px.api('/photos/search', { geo: lat + ',' + lng + ',' + radius/1000 + 'km', page: i }, function (response) {
-        photo_array = response.data.photos.forEach(function(photo){
-          $("<img>").attr("src", photo.image_url).appendTo("#black-box")
-        });
+      // Calculate radius (in meters).
+      var radius = google.maps.geometry.spherical.computeDistanceBetween(center, ne);
+    }
+
+    var time_mod = 0
+    function addRow(response){
+      var row = $("<div>").addClass('row').appendTo("#black-box")
+      photo_array = response.data.photos.forEach(function(photo){
+        $('<img>').attr("src", photo.image_url).addClass('hidden').addClass('transition').appendTo(row)
       });
+      setTimeout(function(){
+        row.children().removeClass('hidden')
+      },720+(80*time_mod))
+      time_mod += 1
+    }
+
+    // $('.row > img').addClass('hidden').fadeOut(2200, function(){
+    //   $(this).remove()
+    // })
+    $('.row').addClass('fast').fadeOut(720, function(){
+      $(this).remove()
+    })
+    // $('.row').each(function(index){
+    //   var row = $(this)
+    //   setTimeout(function(){
+    //     row.addClass('fast').fadeOut(720, function(){
+    //       $(this).remove()
+    //     })
+    //   },80*index)
+    // })
+
+    for(var i = 1; i<=5; i++){
+      _500px.api('/photos/search', { geo: lat + ',' + lng + ',' + radius/1000 + 'km', page: i }, addRow);
     };
   })
-})
 
 
 
